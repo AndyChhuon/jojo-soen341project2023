c2fd33e2fd7273209e806e0c4701120f398b79f6
Camila Vejar <camila.vejar01@gmail.com>
2023-02-20 21:05:45 -0500

    Added more inputs to include in student profile

--- jojo-app/src/pages/studentform.js
+++ jojo-app/src/pages/studentform.js
@@ -8,17 +8,15 @@ export default function StudentForm() {
     firstName: "",
     lastName: "",
     email: "",
-    phone: "",
+    password: "",
     city: "",
     state: "",
     school: "",
-    extracurriculars: "",
-    workExperience: "",
-    awards: "",
-    resume: "",
-    coverLetter: "",
-    linkedIn: "",
-    other: "",
+    rpassword: "",
+    workexp: "",
+    aboutme: "",
+    education: "",
+    username: "",
   });
   const [submitted, setSubmitted] = useState(false);
   const [valid, setValid] = useState(false);
@@ -31,12 +29,10 @@ export default function StudentForm() {
   const handleEmailInputChange = (event) => {
     setStudent({ ...student, email: event.target.value });
   };
-  const handlePhoneInputChange = (event) => {
+  const handlePasswordInputChange = (event) => {
     setStudent({ ...student, phone: event.target.value });
   };
-  const handleAddressInputChange = (event) => {
-    setStudent({ ...student, address: event.target.value });
-  };
+
   const handleCityInputChange = (event) => {
     setStudent({ ...student, city: event.target.value });
   };
@@ -50,6 +46,20 @@ export default function StudentForm() {
   const handleWorkexpInputChange = (event) => {
     setStudent({ ...student, workexp: event.target.value });
   };
+  const handleRpasswordInputChange = (event) => {
+    setStudent({ ...student, rpassword: event.target.value });
+  };
+
+  const handleAboutmeInputChange = (event) => {
+    setStudent({ ...student, aboutme: event.target.value });
+  };
+
+  const handleEducationInputChange = (event) => {
+    setStudent({ ...student, education: event.target.value });
+  };
+  const handleUsernameInputChange = (event) => {
+    setStudent({ ...student, username: event.target.value });
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -62,23 +72,20 @@ export default function StudentForm() {
       <Container>
         <h2>Student Form</h2>
         <form onSubmit={submitHandler}>
-        <div class="container">
-          
-  <div class="row">
-    <div class="col-sm">
-      {/* First of three columns */}
-      {/* <img src="" alt="pfp" class="img-rounded"></img> */}
-      
-<div class="square rounded p-5 bg-secondary">
-  <small>profile image</small>
-</div>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+                {/* First of three columns */}
+                {/* <img src="" alt="pfp" class="img-rounded"></img> */}
 
-    </div>
-    <div class="col-sm">
-      {/* Second of three columns */}
+                <div class="square rounded p-5 bg-secondary">
+                  <small>profile image</small>
+                </div>
+              </div>
+              <div class="col-sm">
+                {/* Second of three columns */}
 
-      <label htmlFor="firstName">First Name: </label>
-              
+                <label htmlFor="firstName">First Name: </label>
                 <input
                   type="text"
                   class="form-control"
@@ -89,45 +96,7 @@ export default function StudentForm() {
                   onChange={handleFirstNameInputChange}
                 />
 
-                <label htmlFor="email">Email: </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  class="form-control"
-                  required
-                  value={student.email}
-                  onChange={handleEmailInputChange}/>
-
-<label htmlFor="city">City: </label>
-              
-              <input
-                type="text"
-                id="city"
-                name="city"
-                class="form-control"
-                required
-                value={student.city}
-                onChange={handleCityInputChange}
-              />
-                
-                <label htmlFor="school">School</label>
-              
-              <input
-                type="text"
-                id="school"
-                name="school"
-                class="form-control"
-                required
-                value={student.school}
-                onChange={handleSchoolInputChange}
-              />
-    </div>
-    <div class="col-sm">
-      {/* Third of three columns */}
-
-        <label htmlFor="lastName">Last Name: </label>
-              
+                <label htmlFor="lastName">Last Name: </label>
                 <input
                   type="text"
                   id="lastName"
@@ -138,66 +107,128 @@ export default function StudentForm() {
                   onChange={handleLastNameInputChange}
                 />
 
-<label htmlFor="phone">Phone Number: </label>
-             
-             <input
-               type="tel"
-               id="phone"
-               name="phone"
-               class="form-control"
-               required
-               value={student.phone}
-               onChange={handlePhoneInputChange}
-             />
+                <label htmlFor="email">Email: </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="form-control"
+                  required
+                  value={student.email}
+                  onChange={handleEmailInputChange}
+                />
 
-<label htmlFor="state">State/Province: </label>
-             
-             <input
-               type="text"
-               id="state"
-               name="state"
-               class="form-control"
-               required
-               value={student.state}
-               onChange={handleStateInputChange}
-             />
- <label htmlFor="school">Other input</label>
-              
-              <input
+                <label htmlFor="password">Password: </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  class="form-control"
+                  required
+                  value={student.password}
+                  onChange={handlePasswordInputChange}
+                />
+              </div>
+              <div class="col-sm">
+                {/* Third of three columns */}
+
+                <label htmlFor="city">City: </label>
+                <input
+                  type="text"
+                  id="city"
+                  name="city"
+                  class="form-control"
+                  required
+                  value={student.city}
+                  onChange={handleCityInputChange}
+                />
+
+                <label htmlFor="state">State/Province: </label>
+                <input
+                  type="text"
+                  id="state"
+                  name="state"
+                  class="form-control"
+                  required
+                  value={student.state}
+                  onChange={handleStateInputChange}
+                />
+
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  class="form-control"
+                  required
+                  value={student.username}
+                  onChange={handleUsernameInputChange}
+                />
+
+                <label htmlFor="rpassword">Re-enter password</label>
+                <input
+                  type="text"
+                  id="rpassword"
+                  name="rpassword"
+                  class="form-control"
+                  required
+                  value={student.rpassword}
+                  onChange={handleRpasswordInputChange}
+                />
+              </div>
+            </div>
+            <div class="form-group">
+              <label htmlFor="workexp">Work Experience: </label>
+              <textarea
                 type="text"
-                id="school"
-                name="school"
+                id="workexp"
+                name="workexp"
                 class="form-control"
+                rows="5"
                 required
-                // value={student.school}
-                // onChange={handleSchoolInputChange}
+                value={student.workexp}
+                onChange={handleWorkexpInputChange}
               />
-    </div>
-  </div>
-  <div class="form-group">
-            <label htmlFor="workexp">Work Experience: </label>
-            <textarea
-              type="text"
-              id="workexp"
-              name="workexp"
-              class="form-control"
-              rows="5"
-              required
-              value={student.workexp}
-              onChange={handleWorkexpInputChange}
-            />
-          </div>
-          <div class="mt-2 col-md-12">
-            <div class="text-center">
-            <div class="d-grid gap-2 d-md-block">
-              <button type="submit" class="btn btn-success  ">
-                Create Profile
-              </button>
+            </div>
+
+            <div class="form-group">
+              <label htmlFor="education">Education: </label>
+              <textarea
+                type="text"
+                id="education"
+                name="education"
+                class="form-control"
+                rows="4"
+                value={student.education}
+                onChange={handleEducationInputChange}
+              />
+            </div>
+
+            <div class="form-group">
+              <label htmlFor="aboutme">About me: </label>
+              <textarea
+                placeholder="Present yourself!"
+                type="text"
+                id="aboutme"
+                name="aboutme"
+                class="form-control"
+                rows="2"
+                value={student.aboutme}
+                onChange={handleAboutmeInputChange}
+              />
+            </div>
+
+            <div class="mt-2 col-md-12">
+              <div class="text-center">
+                <div class="d-grid gap-2 d-md-block">
+                  <button type="submit" class="btn btn-success  ">
+                    Create Profile
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-</div></form>
-        
+        </form>
       </Container>
     </section>
   );
